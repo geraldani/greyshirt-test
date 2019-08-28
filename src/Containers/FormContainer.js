fc2babf27e09fb994c137e31eca6966c9411759d
Geraldyn Chirinos <geraldynchidesousa@gmail.com>
2019-08-28 17:21:52 -0300

    manejando errores de los commits

--- src/Containers/FormContainer.js
+++ src/Containers/FormContainer.js
@@ -36,6 +36,7 @@ class FormContainer extends Component {
             formData: {
                 ...this.state.formData,
                 [e.target.name]: e.target.value,
+                // characters:e.target.name==='description' ? e.target.textLength : this.state.form.characters
             },
             length: {
                 charactersDesc: e.target.name === 'description' ? e.target.textLength : this.state.length.charactersDesc,

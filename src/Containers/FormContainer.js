bc866faaf067d87d4c7a0b435a2a3abe9a900771
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-08-28 16:00:22 -0300

    Creando el componente Lista

--- src/Containers/FormContainer.js
+++ src/Containers/FormContainer.js
@@ -36,7 +36,6 @@ class FormContainer extends Component {
             formData: {
                 ...this.state.formData,
                 [e.target.name]: e.target.value,
-                // characters:e.target.name==='description' ? e.target.textLength : this.state.form.characters
             },
             length: {
                 charactersDesc: e.target.name === 'description' ? e.target.textLength : this.state.length.charactersDesc,
